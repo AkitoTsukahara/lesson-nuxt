680186c2ecbca892538da4498c92b02e734710be
AkitoTsukahara <tsukahara.akito@gmail.com>
2021-03-18 23:05:43 +0900

    lesson loading

---
+++ pages/posts/_id.vue
@@ -0,0 +1,45 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    name: "_id.vue",
+    loading: {
+      continuous: true
+    },
+    async asyncData(context) {
+      const id = context.params.id
+      try {
+        const post = await context.app.$http.$get(
+          `https://api.nuxtjs.dev/posts/${id}`
+        )
+        return {post}
+      } catch (e) {
+        context.error(e)
+      }
+    },
+    mounted() {
+      this.$nextTick(() => {
+        this.$nuxt.$loading.start()
+        setTimeout(() => this.$nuxt.$loading.finish(), 500)
+      }),
+      window.onNuxtReady(() => {
+        console.log('Nuxt.js is ready and mounted')
+      })
+    }
+
+    // middleware({ store, redirect }) {
+    //   // 分解したオブジェクトからキーを取得する
+    //   const isAuthenticated = store.state.authenticated
+    //   if (!isAuthenticated) {
+    //     return redirect('/login')
+    //   }
+    // }
+
+  }
+</script>
+
+<style scoped>
+
+</style>
